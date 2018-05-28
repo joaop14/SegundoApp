15b5b28ddbf9dab5ca60f4f3572e21b3a3e0c1c5
JOAO PEDRO DE SANTANA SOUZA <joao.pssouza3@senacsp.edu.br>
2018-05-28 13:51:28 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/segundoapp/Register_Activity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.segundoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
